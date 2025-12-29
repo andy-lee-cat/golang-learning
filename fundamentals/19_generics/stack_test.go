55ed09b51c2a1f21f22808f1f3ef3594900617d2
skycat <2048513251@qq.com>
2025-12-29 20:33:34 +0800

    Add 19_generics

---
+++ fundamentals/19_generics/stack_test.go
@@ -0,0 +1,67 @@
+package generics
+
+import "testing"
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		// both of these work
+		// myStackOfInts := new(StackOfInts)
+		myStackOfInts := NewStack[int]()
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := new(StackOfStrings)
+
+		// check stack is empty
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfStrings.Push("123")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfStrings.Push("456")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "456")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "123")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+	t.Run("interface stack DX is horrid", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+
+		AssertEqual(t, firstNum+secondNum, 3)
+	})
+
+}
