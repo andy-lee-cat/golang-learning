1b247dfe210d598f9909c44fa1b10db9e19afade
skycat <2048513251@qq.com>
2025-12-30 16:04:07 +0800

    Refactor: 修改server和test的结构

--- application/server.go
+++ application/server.go
@@ -40,16 +40,3 @@ type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
 }
-
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	return s.scores[name]
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
--- application/server_test.go
+++ application/server_test.go
@@ -7,6 +7,19 @@ import (
 	"testing"
 )
 
+type StubPlayerStore struct {
+	scores   map[string]int
+	winCalls []string
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	return s.scores[name]
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{
